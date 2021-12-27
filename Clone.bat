336cb3ac9195b2dcd0ba7b1d2890143fa7d38d7d
bekkolya <62704279+bekkolya@users.noreply.github.com>
2021-12-28 00:33:15 +0400

    Update Clone.bat

--- Clone.bat
+++ Clone.bat
@@ -1,8 +1,3 @@
 cd "C:\Users\bekk-\source\lab2\"
 rd /s /q "C:\Users\bekk-\source\lab2\spring-petclinic"
-<<<<<<< HEAD
-cd "C:\Users\bekk-\source\lab2\"
 git clone https://github.com/bekkolya/spring-petclinic.git
-=======
-git clone https://github.com/bekkolya/spring-petclinic.git
->>>>>>> 8fe88db1028c73a73eb9bb41068e5c63ccc83a25
